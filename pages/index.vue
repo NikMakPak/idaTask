965f6484d0d2ed767f1c623bc464a173b558668c
nikmak <nicement28@gmail.com>
2022-07-21 08:50:40 +0300

    init: new project

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<main>
+</main>
+  
+</template>
+
+<script>
+export default {
+  components: {  },
+  name: 'IndexPage'
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/variables";
+</style>
