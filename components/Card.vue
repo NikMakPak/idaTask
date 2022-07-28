211d6dbddce85ee80e3ef7adfe5f035e1c052f38
nikmak <nicement28@gmail.com>
2022-07-28 09:43:19 +0300

    feat: add variables and adaptive

--- components/Card.vue
+++ components/Card.vue
@@ -45,13 +45,14 @@ export default {
   cursor: pointer;
   transition: $anim-time - .5s;
   position: relative;
-animation: appear $anim-time;
-background: #FFFEFB;
-box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
-border-radius: 4px;
-max-width: 332px;
+  animation: appear $anim-time;
+  background: $card-bg;
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+  border-radius: 4px;
+  max-width: 332px;
+  max-height: 423px;
   &:hover{
-        box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.08), 0px 0px 10px rgba(0, 0, 0, 0.06);
+      box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.08), 0px 0px 10px rgba(0, 0, 0, 0.06);
 }
 }
 
@@ -70,8 +71,8 @@ max-width: 332px;
     border-radius: 4px 4px 0px 0px;
   img{
     width: 100%;
-        height: 100%;
-        object-fit: cover;
+    height: 100%;
+    object-fit: cover;
   }
 }
 .card__content{
@@ -90,10 +91,10 @@ max-width: 332px;
   right: -10px;
   top: -10px;
   background: center no-repeat url("@/assets/img/delete.svg") #FF8484;
-box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-border-radius: 10px;
-width: 32px;
-height: 32px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  width: 32px;
+  height: 32px;
 }
 .visible{
   display: block;
